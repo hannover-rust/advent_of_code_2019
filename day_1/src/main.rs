74171137bbfad7aee410b6083c4f5f09443abebb
csjchneider2 <cjschneider2@gmail.com>
2019-12-02 21:32:11 +0100

    add loop and iter variations

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,11 +1,29 @@
-
 fn main() {
-    let input_str = include_str!("../input.txt");
-    let input: i32 = input_str.parse().expect("not a valid integer");
-    let part1_answer = part1(input);
-    println!("part1: {}", part1_answer);
+    let input = include_str!("../input.txt");
+    let total_loop = using_loop(input);
+    let total_iter = using_iter(input);
+
+    assert_eq!(total_loop, total_iter);
+    println!("part1: {}", total_iter);
 }
 
+// bonus benchmark tests
+fn using_loop(input: &str) -> i32 {
+    let mut total = 0;
+    for line in input.lines() {
+        let mass: i32 = line.parse().expect("not a valid integer");
+        total += part1(mass)
+    }
+    total
+}
+
+fn using_iter(input: &str) -> i32 {
+    input.lines().into_iter()
+        .map(|line| line.parse::<i32>().expect("not a valid integer"))
+        .fold(0, |acc, mass| acc + part1(mass))
+}
+
+/// Converts input mass to required fuel units
 fn part1(input: i32) -> i32 {
     /* your code here */
 }
